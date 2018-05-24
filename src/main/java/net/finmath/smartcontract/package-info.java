cacb456ea51f67cc017e34ad1dfbc18695bbd3d6
Christian Fries <email@christian-fries.de>
2018-05-25 00:33:35 +0200

    Created project.

---
+++ src/main/java/net/finmath/smartcontract/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 25 May 2018
+ */
+
+/**
+ * @author Christian Fries
+ *
+ */
+package net.finmath.smartcontract;
