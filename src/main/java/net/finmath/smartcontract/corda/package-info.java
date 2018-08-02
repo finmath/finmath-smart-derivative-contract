aa5cfaa05616147e4d78bc36249bc87765a36371
Christian Fries <email@christian-fries.de>
2018-08-02 22:15:02 +0200

    Added package description

---
+++ src/main/java/net/finmath/smartcontract/corda/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 02.08.2018
+ */
+
+/**
+ * Prototype realzing the smart derivative contract on Corda.
+ *
+ * @author Christian Fries
+ */
+package net.finmath.smartcontract.corda;
