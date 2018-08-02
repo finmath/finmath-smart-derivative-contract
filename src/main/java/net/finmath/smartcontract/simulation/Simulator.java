7cc4d52cca4b41c318c82812a5b322343cc2e363
Christian Fries <email@christian-fries.de>
2018-08-02 14:29:11 +0200

    Added JavaDoc.

--- src/main/java/net/finmath/smartcontract/simulation/Simulator.java
+++ src/main/java/net/finmath/smartcontract/simulation/Simulator.java
@@ -1,9 +1,22 @@
 package net.finmath.smartcontract.simulation;
 
+import net.finmath.smartcontract.specifications.SmartContract;
 import net.finmath.time.TimeDiscretization;
 
+/**
+ * Simulation of a smart contract over a series of times.
+ *
+ * @author Christian Fries
+ */
 public class Simulator {
 
-	TimeDiscretization time;
+	private final TimeDiscretization timeDiscretization;
+	private final SmartContract smartContract;
+
+	public Simulator(TimeDiscretization timeDiscretization, SmartContract smartContract) {
+		super();
+		this.timeDiscretization = timeDiscretization;
+		this.smartContract = smartContract;
+	}
 
 }
