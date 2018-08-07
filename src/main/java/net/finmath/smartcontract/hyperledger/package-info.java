65798538d2bd13b41c01f4aef82675a26e8d7219
Christian Fries <email@christian-fries.de>
2018-08-07 23:13:05 +0200

    Added package description.

---
+++ src/main/java/net/finmath/smartcontract/hyperledger/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. Contact: email@christianfries.com.
+ *
+ * Created on 25 May 2018
+ */
+
+/**
+ * Prototype realzing the smart derivative contract on Hyperledger.
+ *
+ * @author Christian Fries
+ * @author Peter Kohl-Landgraf
+ */
+package net.finmath.smartcontract.hyperledger;
