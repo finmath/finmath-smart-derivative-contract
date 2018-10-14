cce739afbbff076bace930fba90956e714024955
Christian Fries <email@christian-fries.de>
2018-10-14 14:53:35 +0200

    Organized imports.

--- src/main/java/net/finmath/smartcontract/oracle/ValuationOraclePlainSwapHistoricScenarios.java
+++ src/main/java/net/finmath/smartcontract/oracle/ValuationOraclePlainSwapHistoricScenarios.java
@@ -4,7 +4,6 @@ package net.finmath.smartcontract.oracle;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
