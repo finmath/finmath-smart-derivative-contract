52ba801261780406e2336da1d809048d023e7192
Christian Fries <email@christian-fries.de>
2018-10-19 07:26:46 +0200

    Added ContractEventTypes.

---
+++ src/main/java/net/finmath/smartcontract/contract/SmartDerivativeContract.java
@@ -0,0 +1,31 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 7 Oct 2018
+ */
+
+package net.finmath.smartcontract.contract;
+
+/**
+ * @author Christian Fries
+ *
+ */
+public class SmartDerivativeContract {
+
+	public enum ContractEvents {
+		INIT,
+		SETTLEMENT,
+		ACCOUNTS_ACCESSIBLE_START,
+		ACCOUNTS_ACCESSIBLE_END,
+		CHECK_MARGIN,
+		MATURED,
+	}
+
+	/**
+	 *
+	 */
+	public SmartDerivativeContract() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
