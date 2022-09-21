3a79df9e6463216c3529f5d10102fc36f4fe2490
deichsteller <daniel.eichsteller@cofinpro.de>
2022-09-21 15:27:34 +0200

    Issue #101 - openapi integration and fixes to pom

--- src/main/java/net/finmath/smartcontract/service/SettlementValuationController.java
+++ src/main/java/net/finmath/smartcontract/service/SettlementValuationController.java
@@ -7,6 +7,7 @@
 
 package net.finmath.smartcontract.service;
 
+import net.finmath.smartcontract.api.SettlementValuationApi;
 import net.finmath.smartcontract.util.SDCConstants;
 import net.finmath.smartcontract.util.SDCDateUtil;
 import net.finmath.smartcontract.util.SDCProperties;
@@ -18,9 +19,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -32,13 +30,13 @@ import java.util.HashMap;
 
 /**
  * Controller for the settlement valuation REST service.
- *
+ * TODO Refactor try/catch once openapi can generate exception handling
  * @author Christian Fries
  * @author Peter Kohl-Landgraf
  * @author Dietmar Schnabel
  */
 @RestController
-public class SettlementValuationController {
+public class SettlementValuationController implements SettlementValuationApi {
 	
 	private  static String curve1 = "NONE";
 	private  static String curve2 = "NONE";
@@ -55,15 +53,9 @@ public class SettlementValuationController {
 	 * @param marketDataAsJson1 Market data Json string
 	 * @param marketDataAsJson2 Market data Json string
 	 * @return String Json representing the valuation.
-	 * @throws IOException the exception
 	 */
-	@PostMapping("/margincalulationForProductOnChain")
-	public ResponseEntity<String> settlementvaluationForProductAsFPML(@RequestParam("marketDataAsJson1") String marketDataAsJson1, @RequestParam("marketDataAsJson2") String marketDataAsJson2, @RequestParam("tradeAsFPML") String tradeAsFPML, @RequestParam("tradeId") String tradeId) throws IOException
+	public ResponseEntity<String> settlementvaluationForProductAsFPMLOnChain(String marketDataAsJson1, String marketDataAsJson2, String tradeAsFPML, String tradeId)
 		{
-		
-				
-		
-		
 		LocalDate ld1 = SDCDateUtil.getDateFromJSON(marketDataAsJson1, SDCConstants.DATE_FORMAT_yyyyMMdd);
 		LocalDate ld2 = SDCDateUtil.getDateFromJSON(marketDataAsJson2, SDCConstants.DATE_FORMAT_yyyyMMdd);
 		
@@ -111,17 +103,20 @@ public class SettlementValuationController {
 	 * @param tradeAsFPML Trade FPML file
 	 * @param marketDataAsJson1 Market data Json file1
 	 * @return String Json representing the valuation.
-	 * @throws IOException the exception
 	 */
-	@PostMapping("/margincalulationForProductAsFPMLOneCurve")
-	public ResponseEntity<String> settlementvaluationForProductAsFPMLOneCurve(@RequestParam("marketDataAsJson1") MultipartFile marketDataAsJson1, @RequestParam("tradeAsFPML") MultipartFile tradeAsFPML) throws IOException
+	public ResponseEntity<String> settlementvaluationForProductAsFPMLOneCurve(MultipartFile marketDataAsJson1, MultipartFile tradeAsFPML)
 		{
 		
 		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.add("Responded", "SettlementValuationControllerOneCurve");	
-		
-		String json1String = new String(marketDataAsJson1.getBytes());
-		LocalDate ld1 = SDCDateUtil.getDateFromJSON(json1String, SDCConstants.DATE_FORMAT_yyyyMMdd);
+		responseHeaders.add("Responded", "SettlementValuationControllerOneCurve");
+
+			String json1String = null;
+			try {
+				json1String = new String(marketDataAsJson1.getBytes());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			LocalDate ld1 = SDCDateUtil.getDateFromJSON(json1String, SDCConstants.DATE_FORMAT_yyyyMMdd);
 		String currentDateString = SDCDateUtil.getStringFromDate(ld1, SDCConstants.DATE_FORMAT_yyyyMMdd);
 		
 		LocalDate previousDate = SDCDateUtil.getPreviousBusinessDay(ld1, new BusinessdayCalendarExcludingTARGETHolidays());
@@ -141,13 +136,23 @@ public class SettlementValuationController {
 			return new ResponseEntity<String>(message, responseHeaders, HttpStatus.BAD_REQUEST);
 		}
 		logger.info("Previous File = " + previousFile);
-		String json2String = new String(Files.readAllBytes(previousFile.toPath()));
-		LocalDate ld2 = SDCDateUtil.getDateFromJSON(json2String, SDCConstants.DATE_FORMAT_yyyyMMdd);
-		
+			String json2String = null;
+			try {
+				json2String = new String(Files.readAllBytes(previousFile.toPath()));
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			LocalDate ld2 = SDCDateUtil.getDateFromJSON(json2String, SDCConstants.DATE_FORMAT_yyyyMMdd);
 
-		String fpmlString = new String(tradeAsFPML.getBytes());
-		
-		logger.info("Starting Margin Calculation with dates " + ld1 + " and  " + ld2);
+
+			String fpmlString = null;
+			try {
+				fpmlString = new String(tradeAsFPML.getBytes());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+
+			logger.info("Starting Margin Calculation with dates " + ld1 + " and  " + ld2);
 		MarginCalculator marginCalculator = new MarginCalculator();
 		
 		if (logger.isDebugEnabled()) {
@@ -164,8 +169,12 @@ public class SettlementValuationController {
 		
 		String resultJSON = marginCalculator.getContractValuationAsJSON();
 		logger.info(resultJSON);
-		Files.write( currentFile.toPath(), json1String.getBytes());
-		return new ResponseEntity<String>(resultJSON, responseHeaders, HttpStatus.OK);
+			try {
+				Files.write( currentFile.toPath(), json1String.getBytes());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			return new ResponseEntity<String>(resultJSON, responseHeaders, HttpStatus.OK);
 	}
 	
 	/**
@@ -175,22 +184,31 @@ public class SettlementValuationController {
 	 * @param marketDataAsJson1 Market data Json file1
 	 * @param marketDataAsJson2 Market data Json file2
 	 * @return String Json representing the valuation.
-	 * @throws IOException the exception
 	 */
-	@PostMapping("/margincalulationForProductAsFPML")
-	public ResponseEntity<String> settlementvaluationForProductAsFPML(@RequestParam("marketDataAsJson1") MultipartFile marketDataAsJson1, @RequestParam("marketDataAsJson2") MultipartFile marketDataAsJson2, @RequestParam("tradeAsFPML") MultipartFile tradeAsFPML) throws IOException
+	public ResponseEntity<String> settlementvaluationForProductAsFPML(MultipartFile marketDataAsJson1, MultipartFile marketDataAsJson2, MultipartFile tradeAsFPML)
 		{
-		
-				
-		String json1String = new String(marketDataAsJson1.getBytes());
-		String json2String = new String(marketDataAsJson2.getBytes());
-		String fpmlString = new String(tradeAsFPML.getBytes());
-		
-		
-		
+		String json1String = null;
+		try {
+			json1String = new String(marketDataAsJson1.getBytes());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		String json2String = null;
+		try {
+			json2String = new String(marketDataAsJson2.getBytes());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		String fpmlString = null;
+		try {
+			fpmlString = new String(tradeAsFPML.getBytes());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
 		LocalDate ld1 = SDCDateUtil.getDateFromJSON(json1String, SDCConstants.DATE_FORMAT_yyyyMMdd);
 		LocalDate ld2 = SDCDateUtil.getDateFromJSON(json2String, SDCConstants.DATE_FORMAT_yyyyMMdd);
-		
+
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.add("Responded", "SettlementValuationControllerTwoCurves");
 		boolean b = true;
@@ -205,8 +223,8 @@ public class SettlementValuationController {
 		}
 		logger.info("Starting Margin Calculation with dates " + ld1 + " and  " + ld2);
 		MarginCalculator marginCalculator = new MarginCalculator();
-				
-		
+
+
 		try {
 			marginCalculator.getValue(json1String, json2String, fpmlString);
 		} catch (Exception e) {
@@ -216,27 +234,21 @@ public class SettlementValuationController {
 		if (logger.isDebugEnabled()) {
 			logger.debug("json1bytes: " + json1String);
 			logger.debug("json2bytes: " + json2String);
-			logger.debug("fpmlbytes: " + fpmlString);			
+			logger.debug("fpmlbytes: " + fpmlString);
 		}
 		String resultJSON = marginCalculator.getContractValuationAsJSON();
 		logger.info(resultJSON);
 		return new ResponseEntity<String>(resultJSON, responseHeaders, HttpStatus.OK);
-		
 	}
-
-	
 
 	/**
 	 * Request mapping for lastmargincalculationfpml
 	 * 
 	 * @param tradeId Trade ID
 	 * @return String representing trade as fpml.
-	 * @throws IOException the exception
 	 */
-	@GetMapping("/lastmargincalculationfpml")
-	public ResponseEntity<String> settlementvaluationProductAsFPML(@RequestParam("tradeId") String tradeId) throws IOException
+	public ResponseEntity<String> settlementvaluationProductAsFPML(String tradeId)
 		{
-				
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
 		responseHeaders.add("Responded", "lastmargincalculationfpml");
@@ -244,17 +256,13 @@ public class SettlementValuationController {
 				
 		return new ResponseEntity<String>(totalResult, responseHeaders, HttpStatus.OK);
 	}
-	
-	
-	
+
 	/**
 	 * Request mapping for test
 	 * 
 	 * @return String "Connect successful".
-	 * @throws IOException the exception
 	 */
-	@GetMapping("/test")
-	public ResponseEntity<String> test() throws IOException
+	public ResponseEntity<String> test()
 		{
 				
 		HttpHeaders responseHeaders = new HttpHeaders();
