9fc8d79880f94bd36c257bd56ae48ddea48012c1
Christian Fries <email@christian-fries.de>
2022-09-30 11:12:45 +0200

    Added dummy fail test to test build/integration server.

---
+++ src/test/java/net/finmath/smartcontract/demo/DemoTest.java
@@ -0,0 +1,14 @@
+package net.finmath.smartcontract.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+	@Test
+	public void test() {
+
+		// Try fail here to test the build in the integration server.
+		Assertions.fail();
+	}
+}
