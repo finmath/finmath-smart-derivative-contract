3ffc1c0b95c054d56fb1eb22fab8d6bffeebb331
Christian Fries <email@christian-fries.de>
2022-09-30 20:41:51 +0200

    Client can use user:password as argument.

--- scripts/test-margin-valuation-oracle.sh
+++ scripts/test-margin-valuation-oracle.sh
@@ -5,4 +5,4 @@ SCRIPT_PATH="$(dirname "$0")"
 cd $SCRIPT_PATH/../
 echo $PWD
 
-mvn exec:java -Dexec.mainClass=net.finmath.smartcontract.client.ValuationClient
+mvn exec:java -Dexec.mainClass=net.finmath.smartcontract.client.ValuationClient $1
