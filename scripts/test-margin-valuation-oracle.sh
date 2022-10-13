9803908df1e849dbafe7085a9d7dfec8a410855c
Christian Fries <email@christian-fries.de>
2022-10-13 16:50:00 +0200

    Updating script paths (using bash).

--- scripts/test-margin-valuation-oracle.sh
+++ scripts/test-margin-valuation-oracle.sh
@@ -1,8 +1,11 @@
-echo "Note: The valuation oracle has to run."
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo $SCRIPT_PATH
 
 # Move to top level
-SCRIPT_PATH="$(dirname "$0")"
 cd $SCRIPT_PATH/../
 echo $PWD
 
-mvn compile exec:java -Dexec.mainClass=net.finmath.smartcontract.client.ValuationClient -Dexec.args="$1 $2"
+# Start
+mvn clean compile exec:java -Dexec.mainClass=net.finmath.smartcontract.client.ValuationClient -Dexec.args="$1 $2"
