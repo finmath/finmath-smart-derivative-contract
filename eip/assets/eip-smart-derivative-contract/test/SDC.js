8690333c9eb8ea6b9c33fd6b63435258fbaedae9
pekola <pekola@mailbox.org>
2022-12-07 22:19:50 +0100

    added mintable token mock contract and first test sceleton. works with <npm run test>

--- eip/assets/eip-smart-derivative-contract/test/SDC.js
+++ eip/assets/eip-smart-derivative-contract/test/SDC.js
@@ -1,8 +1,8 @@
+const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const mlog = require('mocha-logger');
 const AbiCoder = ethers.utils.AbiCoder;
 const Keccak256 = ethers.utils.keccak256;
-
 
 describe("SDC functionaly as ERC1155 Token", () => {
 
@@ -21,20 +21,20 @@ describe("SDC functionaly as ERC1155 Token", () => {
     tokenManager = _tokenManager;
     counterparty1 = _counterparty1;
     counterparty2 = _counterparty2;
-    const ERC20Factory = await ethers.getContractFactory("LiquidityToken");
+    const ERC20Factory = await ethers.getContractFactory("SDCToken");
     const SDCER20Factory = await ethers.getContractFactory("SDC");
-    token = await ERC20Factory.deploy("LiquidityToken","LIT");
+    token = await ERC20Factory.deploy();
     await token.deployed();
-    token.mint()
 
-    sdc_object = await SDCER20Factory.deploy(counterparty1.address, counterparty2.address, erc20_token.address,1000,100);
+    sdc_object = await SDCER20Factory.deploy(counterparty1.address, counterparty2.address,counterparty1.address, token.address,1000,100);
 //    erc20_token = await ERC20Factory.deploy("LiquidityToken","LIT");
     await sdc_object.deployed();
     console.log("SDC Address: %s", sdc_object.address);
   });
 
   it("Counterparty1 incepts a trade", async () => {
-     const incept_call = await sdc_object.connect(counterparty1).inceptTrade(fpml_data);
+     const incept_call = await sdc_object.connect(counterparty1).inceptTrade(fpml_data,"blub");
      console.log("TradeId: %s", await sdc_object.callStatic.getTradeState());
      expect(incept_call).to.emit(sdc_object, "TradeInceptedEvent");
    });
+});
