21251c53a614e4b977e4d5541f884097f45ac181
MARCIN STAWICKI <m.stawicki.extern_ohne_vollmacht@dzbank.de>
2023-07-11 16:44:57 +0200

    single-spa

--- frontend/src/app/pages/plain-swap-editor/plain-swap-editor-form/plain-swap-editor-form.component.ts
+++ frontend/src/app/pages/plain-swap-editor/plain-swap-editor-form/plain-swap-editor-form.component.ts
@@ -1000,7 +1000,8 @@ export class PlainSwapEditorFormComponent implements OnInit, AfterViewInit {
     fd.append("tradeData", fileToUpload as Blob);
     this.httpClient
       .post<string>(
-        "http://34.159.3.234:8080/plain-swap-editor/upload-market-data",
+        //"http://34.159.3.234:8080/plain-swap-editor/upload-market-data",
+        "http://localhost:8080/plain-swap-editor/upload-market-data",
         fd,
         {
           headers: new HttpHeaders({
