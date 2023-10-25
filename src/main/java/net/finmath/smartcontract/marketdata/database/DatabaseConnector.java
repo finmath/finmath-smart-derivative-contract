63c50075753e70833bd040b8c062d8857595c54a
Christian Fries <email@christian-fries.de>
2023-10-25 18:47:21 +0200

    Fixed unit test.

--- src/main/java/net/finmath/smartcontract/marketdata/database/DatabaseConnector.java
+++ src/main/java/net/finmath/smartcontract/marketdata/database/DatabaseConnector.java
@@ -5,6 +5,7 @@ import net.finmath.smartcontract.service.utils.ResourceGovernor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -26,6 +27,7 @@ import static java.util.stream.Collectors.joining;
  * @author Luca Bressan
  */
 @Service
+@Profile("!test")
 public class DatabaseConnector {
 
 
