5ff539dde0d08ec71cc9a8398d7d516ca1f1b754
pekola <pekola@mailbox.org>
2024-02-13 09:22:55 +0100

    enhanced unit test logging

--- eip/assets/development/test/DvP.js
+++ eip/assets/development/test/DvP.js
@@ -17,12 +17,14 @@ const abiCoder = new AbiCoder();
   let id;
   let assetAmount = 10000;
   let paymentAmount = 9000;
+  let secWait = 4000;
 
   before(async () => {
-  //https://www.geeksforgeeks.org/node-js-crypto-publicencrypt-method/
-
-    console.log("PublicKey of DvP Oracle: %s",dvpOracle.publicKey);
-
+  //https://www.geeksforgeeks.org/node-js-crypto-publicencrypt-method
+    console.log("====================================================================================================");
+    console.log("PublicKey of DvP Oracle is: %s",dvpOracle.publicKey);
+    console.log("====================================================================================================");
+    await new Promise((resolve) => setTimeout(resolve, secWait));
     const [_buyer, _seller] = await ethers.getSigners();
     buyer = _buyer;
     seller = _seller;
@@ -33,37 +35,69 @@ const abiCoder = new AbiCoder();
     paymentContract = await paymentContractFactory.deploy(buyer.address,seller.address);
     await deliveryContract.deployed();
     await paymentContract.deployed();
-    console.log("DeliveryContract Address: %s", deliveryContract.address);
-    console.log("PaymentContract Address: %s", paymentContract.address);
+    console.log("AssetContract is now deployed at Address: %s", deliveryContract.address);
+    console.log("PaymentContract is now deployed at Address: %s", paymentContract.address);
+    console.log("====================================================================================================");
+    await new Promise((resolve) => setTimeout(resolve, secWait));
   });
 
   it("Incept Trade from Seller", async () => {
+     console.log("1. Incept a Trade by Security Seller");
+     await new Promise((resolve) => setTimeout(resolve, secWait));
+     console.log("====================================================================================================");
+     console.log("- At first Seller generates a hashed key for the Buyer with solidity keccak256");
+     await new Promise((resolve) => setTimeout(resolve, secWait));
      let hashedKeySeller = ethers.utils.solidityKeccak256(["string"],["keySeller"]);
-     console.log("KeyHashedSeller: %s",hashedKeySeller);
+     console.log("- Hashed Key for Buyer is: %s",hashedKeySeller);
+     await new Promise((resolve) => setTimeout(resolve, secWait));
+     console.log("- Seller now calls 'inceptTrade' against the AssetContract providing trade data and hashed key for seller");
+     await new Promise((resolve) => setTimeout(resolve, secWait));
      const call = await deliveryContract.connect(seller).inceptTrade(buyer.address, trade_data,-1000, 998, hashedKeySeller);
      const res = await call.wait();
-
      id = res.events[0].args[0];
-     console.log("TransactionHash:", id);
+     console.log("- The Contract emits a unique Transaction ID:", id);
      await expect(call).to.emit(deliveryContract, "TradeIncepted").withArgs(id,buyer.address);
 
+     await new Promise((resolve) => setTimeout(resolve, secWait));
+     console.log("- Trade is now 'incepted' and hashed key for buyer is stored.");
+     console.log("====================================================================================================");
   });
 
   it("Confirm Trade from Buyer", async () => {
-       let hashedKeyBuyer = ethers.utils.solidityKeccak256(["string"],["keyBuyer"]);
-       console.log("KeyHashedBuyer: %s",hashedKeyBuyer);
+      await new Promise((resolve) => setTimeout(resolve, secWait));
+      console.log("====================================================================================================");
+      console.log("2. Confirm the Trade by Security Buyer");
+      console.log("====================================================================================================");
+      await new Promise((resolve) => setTimeout(resolve, secWait));
+      console.log("- Buyer now generates a hashed key for the Seller with solidity keccak256");
+      await new Promise((resolve) => setTimeout(resolve, secWait));
+      let hashedKeyBuyer = ethers.utils.solidityKeccak256(["string"],["keyBuyer"]);
+      console.log("- Hashed Key for Seller is: %s",hashedKeyBuyer);
+
+      console.log("- Buyer now calls 'confirmTrade' against the AssetContract providing trade details and hashed key for seller");
        const call = await deliveryContract.connect(buyer).confirmTrade(seller.address, trade_data, 1000, 998, hashedKeyBuyer);
 
        await expect(call).to.emit(deliveryContract, "TradeConfirmed");
+      await new Promise((resolve) => setTimeout(resolve, secWait));
+      console.log("- Contract performs trade data matching. Trade is now 'confirmed' and hashed key for seller is stored.");
+      console.log("====================================================================================================");
   });
 
 
  it("Incept Transfer from Buyer", async () => {
+     await new Promise((resolve) => setTimeout(resolve, secWait));
+     console.log("====================================================================================================");
+     console.log("3. Buyer incepts the DvP-Transfer against the Asset Contract by providing the encrypted key");
+     console.log("====================================================================================================");
+     await new Promise((resolve) => setTimeout(resolve, secWait));
      let keyEncryptedBuyer = await EthCrypto.encryptWithPublicKey(dvpOracle.publicKey,"keyBuyer");
      let keyEncrypedAsString = await EthCrypto.cipher.stringify(keyEncryptedBuyer);
-     console.log("KeyEncryptedSeller: %s",keyEncrypedAsString);
+     console.log("- Buyer generates encrypted key for the seller by using public key of DvP Oracle: %s",keyEncrypedAsString);
+     await new Promise((resolve) => setTimeout(resolve, secWait));
+     console.log("- Buyer calls 'inceptTransfer' against AssetContract providing the encrypted key for Seller");
      const call = await deliveryContract.connect(buyer).inceptTransfer(id, assetAmount, seller.address, keyEncrypedAsString) ;
      await expect(call).to.emit(deliveryContract, "AssetTransferIncepted");
+     await new Promise((resolve) => setTimeout(resolve, secWait));
   });
 
 
