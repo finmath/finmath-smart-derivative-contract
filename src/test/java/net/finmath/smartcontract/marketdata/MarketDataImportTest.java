20bd89cc7dff7109d68886d3c509253242149716
pekola <pekola@mailbox.org>
2024-03-04 18:14:49 +0100

    revised unit-test for symbol parsing and market-data-file reading

--- src/test/java/net/finmath/smartcontract/marketdata/MarketDataImportTest.java
+++ src/test/java/net/finmath/smartcontract/marketdata/MarketDataImportTest.java
@@ -9,38 +9,41 @@ import net.finmath.smartcontract.product.SmartDerivativeContractDescriptor;
 import net.finmath.smartcontract.product.xml.SDCXMLParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 
 
 public class MarketDataImportTest {
 
 	@Test
-	void testParseSymbols() throws Exception {
+	void testParseSymbols()  {
 		try {
-			String sdcXML = new String(MarketDataImportTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.product.xml/smartderivativecontract.xml").readAllBytes(), StandardCharsets.UTF_8);
+			String sdcXML = new String(Objects.requireNonNull(MarketDataImportTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.product.xml/smartderivativecontract.xml")).readAllBytes(), StandardCharsets.UTF_8);
 			SmartDerivativeContractDescriptor sdc = SDCXMLParser.parse(sdcXML);
 			List<CalibrationDataItem.Spec> marketdataItems = sdc.getMarketdataItemList();
 
 			Assertions.assertEquals(marketdataItems.size(), 72);
 		}
-		catch(Exception e){
+		catch(IOException e){
 			Assertions.assertFalse(false);
 		}
 	}
 
 
 	@Test
-	void testImportMarketDataJson() throws Exception {
+	void testImportMarketDataJson(){
 		try {
-			final String jsonStr = new String(MarketDataImportTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.client/md_testset1.json").readAllBytes(), StandardCharsets.UTF_8);
+			final String jsonStr = new String(Objects.requireNonNull(MarketDataImportTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.client/md_testset1.json")).readAllBytes(), StandardCharsets.UTF_8);
 
 			List<CalibrationDataset> scenarioList = CalibrationParserDataItems.getScenariosFromJsonString(jsonStr);
 			int setSize = scenarioList.get(0).getDataPoints().size();
 			Assertions.assertEquals(setSize, 71);
 		}
-		catch(Exception e){
+		catch(IOException e){
 			Assertions.assertFalse(false);
 		}
 	}
