e4c7225f6a11d293e3297d505f042423db590d24
MARCIN STAWICKI <m.stawicki.extern_ohne_vollmacht@dzbank.de>
2024-05-24 12:35:19 +0200

    455-Frontend-Finmath

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { ExtraOptions, RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { EmptyRouteComponent } from "./empty-route/empty-route.component";
 import { APP_BASE_HREF } from "@angular/common";
-import { singleSpaPropsSubject } from "src/single-spa/single-spa-props";
 
 let isStandalone = true;
-singleSpaPropsSubject.subscribe((props: any) => {
+/*singleSpaPropsSubject.subscribe((props: any) => {
   isStandalone = props.standalone;
-});
+});*/
 let rts: Routes = [];
 rts = [
   { path: "", redirectTo: "pages", pathMatch: "full" },
