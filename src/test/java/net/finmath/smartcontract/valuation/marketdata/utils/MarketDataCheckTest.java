22037b45baf054abc682810d49095c7a3904983b
Julius Lauterbach <julius.lauterbach@t-online.de>
2024-08-02 15:39:27 +0200

    SDC-543, extended one MarketDataCheckTest with multiple files

--- src/test/java/net/finmath/smartcontract/valuation/marketdata/utils/MarketDataCheckTest.java
+++ src/test/java/net/finmath/smartcontract/valuation/marketdata/utils/MarketDataCheckTest.java
@@ -6,13 +6,22 @@ import net.finmath.smartcontract.product.xml.Smartderivativecontract;
 import net.finmath.smartcontract.settlement.Settlement;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MarketDataCheckTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(MarketDataCheckTest.class);
+
+	private final List<String> files = List.of("marketdata_2008-05-02.xml", "marketdata_2008-05-05.xml", "marketdata_2008-05-06.xml", "marketdata_2008-05-07.xml", "marketdata_2008-05-08.xml", "marketdata_2008-05-09.xml", "marketdata_2008-06-18.xml", "marketdata_2008-06-19.xml", "marketdata_2008-06-20.xml", "marketdata_2008-06-23.xml", "marketdata_2008-06-24.xml", "marketdata_2008-06-25.xml", "marketdata_2008-06-26.xml", "marketdata_2008-06-27.xml", "marketdata_2008-06-30.xml",
+			"marketdata_2008-07-01.xml", "marketdata_2008-07-02.xml", "marketdata_2008-09-09.xml", "marketdata_2008-09-10.xml", "marketdata_2008-10-10.xml", "marketdata_2008-10-13.xml", "marketdata_2008-10-14.xml", "marketdata_2008-10-15.xml");
+
 
 	@Test
 	void checkMarketData_negativeCase() throws IOException {
@@ -44,13 +53,23 @@ class MarketDataCheckTest {
 	@Test
 	void checkMarketData_positiveCase_historical() throws IOException {
 		final String fpml = new String(MarketDataCheckTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.product.xml/smartderivativecontract_simulated_historical_marketdata.xml").readAllBytes(), StandardCharsets.UTF_8);
-		final String md = new String(MarketDataCheckTest.class.getClassLoader().getResourceAsStream("net/finmath/smartcontract/valuation/historicalMarketData/marketdata_2008-09-01.xml").readAllBytes(), StandardCharsets.UTF_8);
 
-		MarketDataList mdl = SDCXMLParser.unmarshalXml(md, MarketDataList.class);
-		Smartderivativecontract sdc = SDCXMLParser.unmarshalXml(fpml, Smartderivativecontract.class);
+		for(String fileName : files){
 
-		MarketDataErrors errors = MarketDataCheck.checkMarketData(mdl, sdc);
-		assertFalse(errors.hasErrors());
-		assertEquals(0, errors.getMissingDataPoints().size());
+			String mdPath = "net/finmath/smartcontract/valuation/historicalMarketData/" +  fileName;
+			final String md = new String(MarketDataCheckTest.class.getClassLoader().getResourceAsStream(mdPath).readAllBytes(), StandardCharsets.UTF_8);
+
+			logger.info("checking file: {}", fileName);
+
+			MarketDataList mdl = SDCXMLParser.unmarshalXml(md, MarketDataList.class);
+			Smartderivativecontract sdc = SDCXMLParser.unmarshalXml(fpml, Smartderivativecontract.class);
+
+			MarketDataErrors errors = MarketDataCheck.checkMarketData(mdl, sdc);
+			assertFalse(errors.hasErrors());
+			assertEquals(0, errors.getMissingDataPoints().size());
+		}
 	}
+
+
+
 }
