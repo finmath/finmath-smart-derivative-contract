ed38fc435355edf3545d9b6e3b6721ad74504358
Julius Lauterbach <git@juliuslauterbach.de>
2024-09-26 17:28:28 +0200

    SDC-626, new SettlementControllerTest, increased logging for SettlementController

--- src/main/java/net/finmath/smartcontract/valuation/service/controllers/SettlementController.java
+++ src/main/java/net/finmath/smartcontract/valuation/service/controllers/SettlementController.java
@@ -6,24 +6,29 @@ import net.finmath.smartcontract.model.InitialSettlementResult;
 import net.finmath.smartcontract.model.RegularSettlementRequest;
 import net.finmath.smartcontract.model.RegularSettlementResult;
 import net.finmath.smartcontract.valuation.service.utils.SettlementService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SettlementController implements SettlementApi {
 
+	private static final Logger logger = LoggerFactory.getLogger(SettlementController.class);
 	private final SettlementService settlementService;
 
 	public SettlementController(SettlementService settlementService) {this.settlementService = settlementService;}
 
 	@Override
 	public ResponseEntity<RegularSettlementResult> generateRegularSettlementResult(RegularSettlementRequest regularSettlementRequest) {
+		logger.info("Generating regular settlement result, generateRegularSettlementResult");
 		RegularSettlementResult regularSettlementResult = settlementService.generateRegularSettlementResult(regularSettlementRequest);
 		return ResponseEntity.ok(regularSettlementResult);
 	}
 
 	@Override
 	public ResponseEntity<InitialSettlementResult> generateInitialSettlementResult(InitialSettlementRequest initialSettlementRequest) {
+		logger.info("Generating initial settlement result, generateInitialSettlementResult");
 		InitialSettlementResult initialSettlementResult = settlementService.generateInitialSettlementResult(initialSettlementRequest);
 		return ResponseEntity.ok(initialSettlementResult);
 	}
