a8ed35a7440a6612b2cbb36df95e921803e96db2
Julius Lauterbach <git@juliuslauterbach.de>
2024-10-15 14:58:27 +0200

    SDC-634, new method SettlementService.includeFixingsOfLastSettlement and test to cover the functionality

--- src/test/java/net/finmath/smartcontract/valuation/service/utils/SettlementServiceTest.java
+++ src/test/java/net/finmath/smartcontract/valuation/service/utils/SettlementServiceTest.java
@@ -1,20 +1,19 @@
 package net.finmath.smartcontract.valuation.service.utils;
 
-import net.finmath.smartcontract.model.InitialSettlementRequest;
-import net.finmath.smartcontract.model.InitialSettlementResult;
-import net.finmath.smartcontract.model.SDCException;
+import net.finmath.smartcontract.model.*;
 import net.finmath.smartcontract.valuation.service.config.ValuationConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class SettlementServiceTest {
@@ -32,8 +31,8 @@ class SettlementServiceTest {
 
 		InitialSettlementRequest initialSettlementRequest = new InitialSettlementRequest().tradeData(productXml);
 
-		Mockito.when(valuationConfig.getLiveMarketDataProvider()).thenReturn("refinitiv");
-		Mockito.when(valuationConfig.isLiveMarketData()).thenReturn(true);
+		when(valuationConfig.getLiveMarketDataProvider()).thenReturn("refinitiv");
+		when(valuationConfig.isLiveMarketData()).thenReturn(true);
 
 		assertThrows(SDCException.class, () -> serviceUnderTest.generateInitialSettlementResult(initialSettlementRequest));
 	}
@@ -45,12 +44,72 @@ class SettlementServiceTest {
 
 		InitialSettlementRequest initialSettlementRequest = new InitialSettlementRequest().tradeData(productXml);
 
-		Mockito.when(valuationConfig.getLiveMarketDataProvider()).thenReturn("internal");
-		Mockito.when(valuationConfig.getInternalMarketDataProvider()).thenReturn("internal");
-		Mockito.when(valuationConfig.isLiveMarketData()).thenReturn(false);
+		when(valuationConfig.getLiveMarketDataProvider()).thenReturn("internal");
+		when(valuationConfig.getInternalMarketDataProvider()).thenReturn("internal");
+		when(valuationConfig.isLiveMarketData()).thenReturn(false);
 
 		InitialSettlementResult initialSettlementResult = serviceUnderTest.generateInitialSettlementResult(initialSettlementRequest);
 		System.out.println(initialSettlementResult.getGeneratedInitialSettlement());
+	}
+
+	@Test
+	void generateRegularSettlement() throws IOException {
+		String settlementLast = new String(SettlementServiceTest.class.getClassLoader().getResourceAsStream("net/finmath/smartcontract/valuation/client/settlement_testset_initial.xml").readAllBytes(), StandardCharsets.UTF_8);
+
+		InputStream inputStream = SettlementServiceTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.product.xml/smartderivativecontract_simulated_historical_marketdata.xml");
+		String productXml = new String(inputStream.readAllBytes());
+
+		RegularSettlementRequest regularSettlementRequest = new RegularSettlementRequest()
+				.settlementLast(settlementLast)
+				.tradeData(productXml);
+
+		when(valuationConfig.getLiveMarketDataProvider()).thenReturn("internal");
+		when(valuationConfig.getInternalMarketDataProvider()).thenReturn("internal");
+		when(valuationConfig.isLiveMarketData()).thenReturn(false);
+
+		RegularSettlementResult regularSettlementResult = serviceUnderTest.generateRegularSettlementResult(regularSettlementRequest);
+		String settlementString = regularSettlementResult.getGeneratedRegularSettlement();
+		System.out.println(settlementString);
+
+		assertTrue(settlementString.contains("ESTRSWP3Y"));
+		assertTrue(settlementString.contains("ESTRSWP1W"));
+		assertTrue(settlementString.contains("REGULAR"));
+		assertFalse(settlementString.contains("INITIAL"));
+		assertTrue(settlementString.contains("<marginValue>12329.11</marginValue>"));
+		assertTrue(settlementString.contains("<marketData>"));
+		assertTrue(settlementString.contains("<requestTimeStamp>"));
+		assertTrue(settlementString.contains("<item>"));
+		assertTrue(settlementString.contains("<value>"));
+		assertTrue(settlementString.contains("<settlementTimeNext>"));
+		assertTrue(settlementString.contains("<settlementNPVNext>"));
+		assertTrue(settlementString.contains("<settlementNPVPrevious>"));
+		assertTrue(settlementString.contains("<settlementNPV>"));
+		assertTrue(settlementString.contains("<marginLimits>"));
+	}
+
+	@Test
+	void generateRegularSettlement_includes_FixingOfLastSettlement() throws IOException {
+		String settlementLast = new String(SettlementServiceTest.class.getClassLoader().getResourceAsStream("net/finmath/smartcontract/valuation/client/settlement_testset_initial_historical.xml").readAllBytes(), StandardCharsets.UTF_8);
+
+		InputStream inputStream = SettlementServiceTest.class.getClassLoader().getResourceAsStream("net.finmath.smartcontract.product.xml/smartderivativecontract_simulated_historical_marketdata.xml");
+		String productXml = new String(inputStream.readAllBytes());
+
+		RegularSettlementRequest regularSettlementRequest = new RegularSettlementRequest()
+				.settlementLast(settlementLast)
+				.tradeData(productXml);
+
+		when(valuationConfig.getLiveMarketDataProvider()).thenReturn("internal");
+		when(valuationConfig.getInternalMarketDataProvider()).thenReturn("internal");
+		when(valuationConfig.isLiveMarketData()).thenReturn(false);
+
+		RegularSettlementResult regularSettlementResult = serviceUnderTest.generateRegularSettlementResult(regularSettlementRequest);
+		String settlementString = regularSettlementResult.getGeneratedRegularSettlement();
+		System.out.println(settlementString);
+
+		assertTrue(settlementString.contains("<id>EUB6FIX6M</id><value>0.0484</value><timeStamp>20080502-170000</timeStamp>"));
+		assertTrue(settlementString.contains("<id>EUB6FIX6M</id><value>0.0521</value><timeStamp>20080917-170000</timeStamp>"));
+		assertTrue(settlementString.contains("<settlementNPV>-752.94</settlementNPV>"));
+		assertTrue(settlementString.contains("<settlementType>REGULAR</settlementType>"));
 	}
 
 }
